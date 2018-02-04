79e09ade994c5ca649ef8cfd3047f809a482c488
cyraxxenos <cyraxxenos@gmail.com>
2018-02-04 12:55:25 +0300

    Update sel.js

--- sel.js
+++ sel.js
@@ -1,7 +1,7 @@
 var jade = {st:[], d:0, m:0, w:0, hr:0, mn:0, sc:0}
 
 function Sel(j) {var ABC = function(j){return "&#"+ (64+j) +";"},
-	s = ["https://cyraxxenos.github.io",
+	s = ["",
 	"Бараева Виктория Сергеевна",
 	"Головина Юлия Сергеевна",
 	"Евсейчик Полина Алексеевна",
@@ -29,7 +29,7 @@ function Sel(j) {var ABC = function(j){return "&#"+ (64+j) +";"},
 	jade.st[j] ="<br>"+ j +') <select id="sel'+ j +'" onchange="top.location = this.value; this.value=0">'+
 		'<option value='+ s[0] +' selected> </option>';
 	for (var i=1; i<s.length; i++){
-		s[0] = '&#92;'+ ABC(j) +"\\"+ ABC(j) + i +'.html';
+		s[0] = 'https://cyraxxenos.github.io&#92;'+ ABC(j) +"\\"+ ABC(j) + i +'.html';
 		jade.st[j] += '<option value="'+ s[0] +'">'+ (i<10?'  ':'') + i +') '+ s[i] +'</option>';
 	} return (jade.st[j] += "</select><br>");
 }
