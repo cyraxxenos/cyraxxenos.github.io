61bc7e13435e0be7e74041c7aec563f351bf3179
cyraxxenos <cyraxxenos@gmail.com>
2018-02-07 16:14:26 +0300

    Update t.js

--- t.js
+++ t.js
@@ -71,12 +71,14 @@ c["p"]=d
 }}}}
       jade.w = t.getUTCDay()
       jade.hr = t.getUTCHours()
-      //jade.mn = t.getUTCMinutes()
+      jade.mn = t.getUTCMinutes()
       //jade.sc = t.getUTCSeconds()
-      if(jade.hr<10 || jade.hr>18) {ID("r").innerHTML = ""}
-      if(!ID("S2_1") && jade.w==0 && jade.hr>=10 && jade.hr<12) { ID("r").innerHTML = ("Варианты по фамилиям (2 курс) для ЛР:<br>"+ S2(1)) }
-      if(!ID("S4_1") && jade.w==0 && jade.hr>=12 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
-      if(!ID("S4_2") && jade.w==0 && jade.hr>=15 && jade.hr<18) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)+S4(2)) }
+      if(jade.hr<11 || jade.hr>=15) {ID("r").innerHTML = ""}
+      if(!ID("S2_1") && jade.w==2 && jade.hr==11 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (2 курс) для ЛР:<br>"+ S2(1)) }
+      if(!ID("S4_1") && jade.w==4 && jade.hr==11 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
+      if(!ID("S4_1") && jade.w==4 && jade.hr>=11 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
+      if(!ID("S4_1") && jade.w==3 && jade.hr==16 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
+      //if(!ID("S4_2") && jade.w==4 && jade.hr>=11 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)+S4(2)) }
 tout=setTimeout('time100.tick("")',updint-tU%updint)
 }
 function l0(n){return n>9?n:"0"+n}
