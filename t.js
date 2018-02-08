a2333c6b465b17f75eaa3bc6f26d46cbb975c927
cyraxxenos <cyraxxenos@gmail.com>
2018-02-08 04:00:04 +0300

    A

--- t.js
+++ t.js
@@ -73,9 +73,9 @@ c["p"]=d
       jade.hr = t.getUTCHours()
       jade.mn = t.getUTCMinutes()
       //jade.sc = t.getUTCSeconds()
-      //if(jade.hr<11 || jade.hr>=15) {ID("r").innerHTML = ""}
+      if(jade.hr<11 || jade.hr>=15) {ID("r").innerHTML = ""}
       if(!ID("S2_1") && jade.w==2 && jade.hr==11 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (2 курс) для ЛР:<br>"+ S2(1)) }
-      if(!ID("S4_1") && jade.w==4 && jade.hr>=2 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
+      if(!ID("S4_1") && jade.w==4 && jade.hr==11 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
       if(!ID("S4_1") && jade.w==4 && jade.hr>11 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
       //if(!ID("S4_2") && jade.w==4 && jade.hr>=11 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)+S4(2)) }
 tout=setTimeout('time100.tick("")',updint-tU%updint)
