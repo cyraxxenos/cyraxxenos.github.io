5d5c43dd82edfef82dcb170b4837135cebcc2a36
cyraxxenos <cyraxxenos@gmail.com>
2018-02-13 00:57:06 +0300

    Update t.js

--- t.js
+++ t.js
@@ -74,7 +74,7 @@ c["p"]=d
       jade.mn = t.getUTCMinutes()
       //jade.sc = t.getUTCSeconds()
       if(jade.hr<11 || jade.hr>=15) {ID("r").innerHTML = ""}
-      if(!ID("S2_1") && jade.w==2 && jade.hr==11 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (2 курс) для ЛР:<br>"+ S2(1)) }
+      if(!ID("S2_1") && jade.w==2 && jade.hr==11 && jade.mn==30) { ID("r").innerHTML = ("Варианты по фамилиям (2 курс) для ЛР:<br>"+ S2(1)) }
       if(!ID("S4_1") && jade.w==4 && jade.hr==11 && jade.mn>30) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
       if(!ID("S4_1") && jade.w==4 && jade.hr>11 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)) }
       //if(!ID("S4_2") && jade.w==4 && jade.hr>=11 && jade.hr<15) { ID("r").innerHTML = ("Варианты по фамилиям (4 курс) для ЛР:<br>"+ S4(1)+S4(2)) }
