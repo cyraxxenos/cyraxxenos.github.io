6682aa1f188e25b565694e92be36a167af573540
cyraxxenos <cyraxxenos@gmail.com>
2018-03-20 18:58:02 +0300

    A

---
+++ gis/data/OGRGeoJSONLineString_1.js
@@ -0,0 +1,9 @@
+var json_OGRGeoJSONLineString_1 = {
+"type": "FeatureCollection",
+"name": "OGRGeoJSONLineString_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "type": null }, "geometry": { "type": "LineString", "coordinates": [ [ 39.830079680402292, 59.212805672401629 ], [ 39.830208088319417, 59.212057402333905 ] ] } },
+{ "type": "Feature", "properties": { "type": null }, "geometry": { "type": "LineString", "coordinates": [ [ 39.830178455723164, 59.212820839868556 ], [ 39.830326618704447, 59.212067514200896 ] ] } }
+]
+}
