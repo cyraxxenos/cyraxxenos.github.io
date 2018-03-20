fb2d85220b7099665704f7cd6722a50bdac102de
cyraxxenos <cyraxxenos@gmail.com>
2018-03-20 19:07:22 +0300

    A

---
+++ gis/data/_4.js
@@ -0,0 +1,8 @@
+var json__4 = {
+"type": "FeatureCollection",
+"name": "_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "type": "LineString" }, "geometry": { "type": "LineString", "coordinates": [ [ 39.906707148434329, 59.210427294086301 ], [ 39.908014960740715, 59.209858914287445 ], [ 39.908369337137145, 59.20954123016768 ], [ 39.907010527472082, 59.210179906290989 ], [ 39.906707148434329, 59.210427294086301 ] ] } }
+]
+}
