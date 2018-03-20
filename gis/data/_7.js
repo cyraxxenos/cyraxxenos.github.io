4befbca3926b6706ba2634ccde3b86ecc82f5ab4
cyraxxenos <cyraxxenos@gmail.com>
2018-03-20 19:02:53 +0300

    A

---
+++ gis/data/_7.js
@@ -0,0 +1,10 @@
+var json__7 = {
+"type": "FeatureCollection",
+"name": "_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "type": "LineString" }, "geometry": { "type": "LineString", "coordinates": [ [ 39.909285115670386, 59.208717247203992 ], [ 39.909212891121541, 59.208824941303035 ] ] } },
+{ "type": "Feature", "properties": { "type": "LineString" }, "geometry": { "type": "LineString", "coordinates": [ [ 39.909395994725905, 59.208780657037678 ], [ 39.909212891121541, 59.208824941303035 ], [ 39.908539990091661, 59.20945576281715 ], [ 39.908014960740715, 59.209858914287445 ], [ 39.906707148434329, 59.210427294086301 ], [ 39.905591791194411, 59.210907782012704 ], [ 39.905437855887328, 59.210787154851346 ] ] } },
+{ "type": "Feature", "properties": { "type": "LineString" }, "geometry": { "type": "LineString", "coordinates": [ [ 39.905591791194411, 59.210907782012704 ], [ 39.905356145129076, 59.210836559020656 ] ] } }
+]
+}
---
+++ gis/data/_9.js
@@ -0,0 +1,16 @@
+var json__9 = {
+"type": "FeatureCollection",
+"name": "_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Обозначение": "СЗ-337" }, "geometry": { "type": "Point", "coordinates": [ 39.909285115670386, 59.208717247203992 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "СЗ-336" }, "geometry": { "type": "Point", "coordinates": [ 39.909395994725905, 59.208780657037678 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "P1 " }, "geometry": { "type": "Point", "coordinates": [ 39.909212891121541, 59.208824941303035 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "P2 " }, "geometry": { "type": "Point", "coordinates": [ 39.908539990091661, 59.20945576281715 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "P3 " }, "geometry": { "type": "Point", "coordinates": [ 39.908014960740715, 59.209858914287445 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "P4 " }, "geometry": { "type": "Point", "coordinates": [ 39.906707148434329, 59.210427294086301 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "P5 " }, "geometry": { "type": "Point", "coordinates": [ 39.905591791194411, 59.210907782012704 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "СЗ-334" }, "geometry": { "type": "Point", "coordinates": [ 39.905437855887328, 59.210787154851346 ] } },
+{ "type": "Feature", "properties": { "Обозначение": "СЗ-335" }, "geometry": { "type": "Point", "coordinates": [ 39.905356145129076, 59.210836559020656 ] } }
+]
+}
