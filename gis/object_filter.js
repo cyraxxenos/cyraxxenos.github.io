d23a974e115841d0f87cf9c5258b03a98717ad9d
cyraxxenos <cyraxxenos@gmail.com>
2018-03-25 00:21:09 +0300

    A

--- gis/object_filter.js
+++ gis/object_filter.js
@@ -2,13 +2,13 @@ ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map('map', {
-            center: [59.21156, 39.83260],
-            zoom: 17,
-            type: 'yandex#satellite',
-            controls: ['zoomControl', 'typeSelector',  'fullscreenControl']
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+		center: [59.21156, 39.83260],
+		zoom: 17,
+		type: 'yandex#satellite',
+		controls: ['zoomControl', 'typeSelector', 'fullscreenControl']
+	}, {
+		searchControlProvider: 'yandex#search'
+	}),
         objectManager = new ymaps.ObjectManager({
             // Чтобы метки начали кластеризоваться, выставляем опцию.
 		clusterize: true,
@@ -17,58 +17,51 @@ function init() {
 		maxZoom: 17,
             // Макет метки кластера pieChart.
 		clusterIconLayout: "default#pieChart"
-        });
+	});
     myMap.geoObjects.add(objectManager);
 
     // Создадим 5 пунктов выпадающего списка.
-    var listBoxItems = ['Вяз', 'Ясень', 'Липа', 'Ольха', 'Яблоня', 'Ива', 'Клён', 'Рябина', 'Тополь', 'Другие']
-        .map(function(title) {
+    var listBoxItems = ['Вяз','Ясень','Липа','Ольха','Яблоня','Ива','Клён','Рябина','Тополь','Другие']
+	.map(function(title) {
             return new ymaps.control.ListBoxItem({
-                data: {
-                    content: title
-                },
-                state: {
-                    selected: true
-                }
+		data: {content: title},
+		state: {selected: true}
             })
-        }),
+	}),
         // Теперь создадим список, содержащий 5 пунктов.
-        listBoxControl = new ymaps.control.ListBox({
-            data: {
-                content: 'Фильтр',
-                title: 'Фильтр'
-            },
-            items: listBoxItems,
-            state: {
-                // Признак, развернут ли список.
-                expanded: true,
-                filters: listBoxItems.reduce(function(filters, filter) {
-                    filters[filter.data.get('content')] = filter.isSelected();
-                    return filters;
-                }, {})
-            }
-        });
+	listBoxControl = new ymaps.control.ListBox({
+		data: {content:'Фильтр', title:'Фильтр'},
+		items: listBoxItems,
+		state: {
+	                // Признак, развернут ли список.
+	                expanded: true,
+	                filters: listBoxItems.reduce(function(filters, filter) {
+	                    filters[filter.data.get('content')] = filter.isSelected();
+	                    return filters;
+	                }, {})
+		}
+	});
     myMap.controls.add(listBoxControl);
 
     // Добавим отслеживание изменения признака, выбран ли пункт списка.
     listBoxControl.events.add(['select', 'deselect'], function(e) {
-        var listBoxItem = e.get('target');
-        var filters = ymaps.util.extend({}, listBoxControl.state.get('filters'));
-        filters[listBoxItem.data.get('content')] = listBoxItem.isSelected();
-        listBoxControl.state.set('filters', filters);
+	var listBoxItem = e.get('target');
+	var filters = ymaps.util.extend({}, listBoxControl.state.get('filters'));
+	filters[listBoxItem.data.get('content')] = listBoxItem.isSelected();
+	listBoxControl.state.set('filters', filters);
     });
 
     var filterMonitor = new ymaps.Monitor(listBoxControl.state);
     filterMonitor.add('filters', function(filters) {
-        // Применим фильтр.
-        objectManager.setFilter(getFilterFunction(filters));
+	// Применим фильтр.
+	objectManager.setFilter(getFilterFunction(filters));
     });
 
     function getFilterFunction(categories){
-        return function(obj){
-            var content = obj.properties.balloonContent;
-            return categories[content]
-        }
+	return function(obj){
+		var content = obj.properties.balloonContent;
+		return categories[content]
+	}
     }
 
     $.ajax({url: "data.json"}).done(function (data) { objectManager.add(data) });
