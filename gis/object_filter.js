7c31d2749beafe5b0668438b5ad2df66742df20f
cyraxxenos <cyraxxenos@gmail.com>
2018-03-25 18:57:30 +0300

    A

--- gis/object_filter.js
+++ gis/object_filter.js
@@ -64,6 +64,7 @@ function init() {
 
     });
 
+
 	myMap.events.add('click', function(e) {myMap.balloon.close()});
 
     var filterMonitor = new ymaps.Monitor(listBoxControl.state);
@@ -80,5 +81,82 @@ function init() {
     }
 
 	$.ajax({url: "data.json"}).done(function (data) { objectManager.add(data) });
+
+	var myCollection = new ymaps.GeoObjectCollection();
+	$.ajax({url: "data.json"}).done(function (data) { myCollection.add(data) });
+	// Добавляем коллекцию меток на карту.
+	myMap.geoObjects.add(myCollection);
+
+    // Создаем экземпляр класса ymaps.control.SearchControl
+    var mySearchControl = new ymaps.control.SearchControl({
+            // Заменяем стандартный провайдер данных (геокодер) нашим собственным.
+            provider: new CustomSearchProvider(myPoints),
+            // Не будем показывать еще одну метку при выборе результата поиска, т.к. метки myCollection уже добавлены на карту.
+            noPlacemark: true,
+            resultsPerPage: 5
+	});
+
+    // Добавляем контрол в верхний правый угол,
+    myMap.controls.add(mySearchControl, { right: 10, top: 50 })
+
+// Провайдер данных для элемента управления ymaps.control.SearchControl.
+// Осуществляет поиск геообъектов в по массиву points.
+// Реализует интерфейс IGeocodeProvider.
+function CustomSearchProvider(points) {this.points = points}
+
+// Провайдер ищет по полю text стандартным методом String.ptototype.indexOf.
+CustomSearchProvider.prototype.geocode = function (request, options) {
+    var promise = new ymaps.util.Promise(),
+        geoObjects = new ymaps.GeoObjectArray(),
+        // Сколько результатов нужно пропустить.
+        offset = options.skip || 0,
+        // Количество возвращаемых результатов.
+        limit = options.results || 20;
+
+    var points = [];
+    // Ищем в свойстве text каждого элемента массива.
+    for (var i = 0, l = this.points.length; i < l; i++) {
+        var point = this.points[i];
+        if (point.text.toLowerCase().indexOf(request.toLowerCase()) != -1) {
+            points.push(point);
+        }
+    }
+    // При формировании ответа можно учитывать offset и limit.
+    points = points.splice(offset, limit);
+    // Добавляем точки в результирующую коллекцию.
+    for (var i = 0, l = points.length; i < l; i++) {
+        var point = points[i],
+            coords = point.coords,
+                    text = point.text;
+
+        geoObjects.add(new ymaps.Placemark(coords, {
+            name: text + ' name',
+            description: text + ' description',
+            balloonContentBody: '<p>' + text + '</p>',
+            boundedBy: [coords, coords]
+        }));
+    }
+
+    promise.resolve({
+        // Геообъекты поисковой выдачи.
+        geoObjects: geoObjects,
+        // Метаинформация ответа.
+        metaData: {
+            geocoder: {
+                // Строка обработанного запроса.
+                request: request,
+                // Количество найденных результатов.
+                found: geoObjects.getLength(),
+                // Количество возвращенных результатов.
+                results: limit,
+                // Количество пропущенных результатов.
+                skip: offset
+            }
+        }
+    });
+
+	// Возвращаем объект-обещание.
+	return promise;
+};
 
 }
