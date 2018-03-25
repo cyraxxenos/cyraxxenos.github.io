b241f3fd76b557db6fe81d711e7b86ff79ec24ba
cyraxxenos <cyraxxenos@gmail.com>
2018-03-25 19:16:36 +0300

    A

--- gis/object_filter.js
+++ gis/object_filter.js
@@ -17,7 +17,17 @@ function init() {
 	// Макет метки кластера pieChart.
 		clusterIconLayout: "default#pieChart"
 	});
-    myMap.geoObjects.add(objectManager);
+
+
+    $.getJSON('data.json')
+        .done(function (geoJson) {
+            // Добавляем описание объектов в формате JSON в менеджер объектов.
+            objectManager.add(geoJson);
+            // Добавляем объекты на карту.
+            myMap.geoObjects.add(objectManager);
+        });
+
+	//myMap.geoObjects.add(objectManager);
 
 	// Создадим 5 пунктов выпадающего списка.
     var listBoxItems = ['Вяз','Ясень','Липа','Ольха','Яблоня','Ива','Клён','Рябина','Тополь','Другие']
@@ -80,83 +90,6 @@ function init() {
 	}
     }
 
-	$.ajax({url: "data.json"}).done(function (data) { objectManager.add(data) });
-
-	var myCollection = new ymaps.GeoObjectCollection();
-	$.ajax({url: "data.json"}).done(function (data) { myCollection.add(data) });
-	// Добавляем коллекцию меток на карту.
-	myMap.geoObjects.add(myCollection);
-
-    // Создаем экземпляр класса ymaps.control.SearchControl
-    var mySearchControl = new ymaps.control.SearchControl({
-            // Заменяем стандартный провайдер данных (геокодер) нашим собственным.
-            provider: new CustomSearchProvider(myPoints),
-            // Не будем показывать еще одну метку при выборе результата поиска, т.к. метки myCollection уже добавлены на карту.
-            noPlacemark: true,
-            resultsPerPage: 5
-	});
-
-    // Добавляем контрол в верхний правый угол,
-    myMap.controls.add(mySearchControl, { right: 10, top: 50 })
-
-// Провайдер данных для элемента управления ymaps.control.SearchControl.
-// Осуществляет поиск геообъектов в по массиву points.
-// Реализует интерфейс IGeocodeProvider.
-function CustomSearchProvider(points) {this.points = points}
-
-// Провайдер ищет по полю text стандартным методом String.ptototype.indexOf.
-CustomSearchProvider.prototype.geocode = function (request, options) {
-    var promise = new ymaps.util.Promise(),
-        geoObjects = new ymaps.GeoObjectArray(),
-        // Сколько результатов нужно пропустить.
-        offset = options.skip || 0,
-        // Количество возвращаемых результатов.
-        limit = options.results || 20;
-
-    var points = [];
-    // Ищем в свойстве text каждого элемента массива.
-    for (var i = 0, l = this.points.length; i < l; i++) {
-        var point = this.points[i];
-        if (point.text.toLowerCase().indexOf(request.toLowerCase()) != -1) {
-            points.push(point);
-        }
-    }
-    // При формировании ответа можно учитывать offset и limit.
-    points = points.splice(offset, limit);
-    // Добавляем точки в результирующую коллекцию.
-    for (var i = 0, l = points.length; i < l; i++) {
-        var point = points[i],
-            coords = point.coords,
-                    text = point.text;
-
-        geoObjects.add(new ymaps.Placemark(coords, {
-            name: text + ' name',
-            description: text + ' description',
-            balloonContentBody: '<p>' + text + '</p>',
-            boundedBy: [coords, coords]
-        }));
-    }
-
-    promise.resolve({
-        // Геообъекты поисковой выдачи.
-        geoObjects: geoObjects,
-        // Метаинформация ответа.
-        metaData: {
-            geocoder: {
-                // Строка обработанного запроса.
-                request: request,
-                // Количество найденных результатов.
-                found: geoObjects.getLength(),
-                // Количество возвращенных результатов.
-                results: limit,
-                // Количество пропущенных результатов.
-                skip: offset
-            }
-        }
-    });
-
-	// Возвращаем объект-обещание.
-	return promise;
-};
+	//$.ajax({url: "data.json"}).done(function (data) { objectManager.add(data) });
 
 }
