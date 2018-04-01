bbd33d1b9a745b43ee53b6652c4a6f7cf7e594d3
cyraxxenos <cyraxxenos@gmail.com>
2018-04-01 18:28:56 +0300

    A

--- gis/object_filter.js
+++ gis/object_filter.js
@@ -97,11 +97,10 @@ function init() {
 
 	//$.ajax({url:"data.json"}).done(function(data) {objectManager.add(data)});
 
-	function fix_repl(a,n) {a = parseFloat(a); return (round(a,n)<0 ? '-':'') + Math.abs(a).toFixed(n).replace('.',',')}
 	function round_4(a) {return Math.round(parseFloat(a)*10000)/10000}
 	function rplN(a) {return (a+'').replace(/[,]/g,'.')}
 	function z2(a){var b=a; if (parseFloat(rplN(a))<10){b ='0'+ b} return b}
-	function deg_dms3(val) {var R = deg_dms(val); return (R[0]==0?R[3]:"")+R[0]+"&#176; "+z2(R[1])+"&#8242; "+z2(fix_repl(R[2],3))+"&#8243;"}
+	function deg_dms3(val) {var R = deg_dms(val); return (R[0]==0?R[3]:"")+R[0]+"&#176; "+z2(R[1])+"&#8242; "+z2(R[2].toFixed(3))+"&#8243;"}
 	function deg_dms(val) {
 		var val_s = (val<0?"-":""), val_u = Math.abs(val);
 		var g = Math.floor(val_u);
