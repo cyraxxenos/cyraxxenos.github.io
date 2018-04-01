27ac6a5f282a6e1987d6ee2362c7f66c8cf63cce
cyraxxenos <cyraxxenos@gmail.com>
2018-04-01 12:17:28 +0300

    A

--- gis/object_filter.js
+++ gis/object_filter.js
@@ -96,7 +96,7 @@ function init() {
     });
 
 	//$.ajax({url:"data.json"}).done(function(data) {objectManager.add(data)});
-	myMap.events.add('contextmenu', function(e) {myMap.balloon.open( e.get('coord'), this.get('coord') )});
+	//myMap.events.add('contextmenu', function(e) {myMap.balloon.open( e.get('coord'), 'coord' )});
 	myMap.events.add('actionbegin', function(e) {getCou()});
 	myMap.events.add('actionbreak', function(e) {getCou()});
 	myMap.events.add('actionend', function(e) {getCou()});
