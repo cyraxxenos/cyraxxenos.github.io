689e5f1ac9548a8f176c4f9b3e6fd48fc5a8a41c
cyraxxenos <cyraxxenos@gmail.com>
2018-06-26 20:04:46 +0300

    A

--- gis/object_filter.js
+++ gis/object_filter.js
@@ -204,6 +204,7 @@ ymaps.mapType.storage.add('Concrete#mapType', new ymaps.MapType('Concrete', ['Co
 		});
 		myMap.geoObjects.add(objectManager);	// Добавляем объекты на карту
 		objectManager.objects.each(function (object) { jdata.push(object.properties); jsdata.push(object) });
+		getCou()
 	});
 
 	$.getJSON('https://cyraxxenos.github.io/gis/dataV.json').done(function (geoJson) {
